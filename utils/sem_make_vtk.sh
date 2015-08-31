10e6548f717e5331cd82da3831e71811706069b5
ktao <taotaokai@gmail.com>
2015-08-31 13:10:36 -0500

    update utils/sem_make_vtk.sh

--- utils/sem_make_vtk.sh
+++ utils/sem_make_vtk.sh
@@ -3,21 +3,27 @@
 #Usage: xcombine_vol_data slice_list filename input_topo_dir input_file_dir 
 #         output_dir high/low-resolution [region]
 
+if [ $# -eq 0 ]
+then
+cat<<EOF
+Usage: $0 <model_tags> <nproc> <mesh_dir> <model_dir> <out_dir>
+EOF
+  exit
+fi
+
 model_name_array=${1:-vp,vs}
-syn_dir=${2:-.}
-model_dir=$syn_dir/${3:-DATABASES_MPI}
-mesh_dir=$syn_dir/${4:-DATABASES_MPI}
-vtk_dir=$syn_dir/${5:-VTK}
+nproc=${2:-1}
+mesh_dir=${3:-DATABASES_MPI}
+model_dir=${4:-DATABASES_MPI}
+vtk_dir=${5:-VTK}
 
 sem_bin=specfem3d_globe/bin
-
-echo "### syn_dir=$syn_dir vtk_dir=$vtk_dir model_name=$model_name_array"
 
 if [ ! -d "$vtk_dir" ]; then
   mkdir $vtk_dir 
 fi
 
-nproc=$(grep NPROCTOT_VAL OUTPUT_FILES/values_from_mesher.h | awk '{print $NF}')
+#nproc=$(grep NPROCTOT_VAL OUTPUT_FILES/values_from_mesher.h | awk '{print $NF}')
 seq 0 $(($nproc - 1)) > $vtk_dir/slice_list
 
 for model_name in $(echo $model_name_array | sed "s/,/ /g")
@@ -32,6 +38,3 @@ do
     $model_dir \
     $vtk_dir 0 1
 done
-
-#bin/xcombine_vol_data_vtk \
-#  VTK/slice_list vs DATABASES_MPI MODEL_SLAB VTK 1 1

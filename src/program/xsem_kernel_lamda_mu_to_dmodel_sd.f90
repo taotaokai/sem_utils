24e3d1b0a0df3b75ff0f737565119287335f5432
ktao <taotaokai@gmail.com>
2015-11-20 02:18:03 -0600

    update xsem_kernel_lamda_mu_to_dmodel_sd.f90
    add model selection

--- src/program/xsem_kernel_lamda_mu_to_dmodel_sd.f90
+++ src/program/xsem_kernel_lamda_mu_to_dmodel_sd.f90
@@ -9,7 +9,7 @@ subroutine selfdoc()
   print '(a)', ""
   print '(a)', "  xsem_kernel_lamda_mu_to_dmodel \"
   print '(a)', "    <nproc> <mesh_dir> <kernel_dir> <out_dir>"
-  print '(a)', "    <scale_factor> <use_kernel_mask> "
+  print '(a)', "    <model_tags> <scale_factor> <use_kernel_mask> "
   print '(a)', ""
   print '(a)', "DESCRIPTION"
   print '(a)', ""
@@ -21,6 +21,7 @@ subroutine selfdoc()
   print '(a)', "  (string) mesh_dir:  directory containing proc000***_reg1_solver_data.bin"
   print '(a)', "  (string) kernel_dir:  directory holds proc*_reg1_[lamda,mu,rho]_kernel.bin"
   print '(a)', "  (string) out_dir:  output directory for dmodel"
+  print '(a)', "  (string) model_tags: comma delimited model names to be used, e.g. mu_kernel,lamda_kernel"
   print '(a)', "  (float) scale_factor:  factor to scale kernel values"
   print '(a)', "  (logical) use_kernel_mask:  flag if kernel mask is used (mask files should locate in kernel_dir)"
 
@@ -40,14 +41,19 @@ program xsem_kernel_lamda_mu_to_dmodel
   !===== declare variables
 
   ! command line args
-  integer, parameter :: nargs = 6
+  integer, parameter :: nargs = 7
   character(len=MAX_STRING_LEN) :: args(nargs)
   integer :: nproc
   character(len=MAX_STRING_LEN) :: mesh_dir
   character(len=MAX_STRING_LEN) :: kernel_dir
   character(len=MAX_STRING_LEN) :: out_dir
+  character(len=MAX_STRING_LEN) :: model_tags
   real(dp) :: scale_factor
   logical :: use_kernel_mask
+
+  ! model names
+  integer :: nmodel
+  character(len=MAX_STRING_LEN), allocatable :: model_names(:)
 
   ! local variables
   integer, parameter :: iregion = IREGION_CRUST_MANTLE ! crust_mantle
@@ -72,8 +78,9 @@ program xsem_kernel_lamda_mu_to_dmodel
   read(args(2),'(a)') mesh_dir 
   read(args(3),'(a)') kernel_dir 
   read(args(4),'(a)') out_dir
-  read(args(5),*) scale_factor 
-  select case (args(6))
+  read(args(5),'(a)') model_tags
+  read(args(6),*) scale_factor 
+  select case (args(7))
     case ('0')
       use_kernel_mask = .false.
     case ('1')
@@ -82,6 +89,13 @@ program xsem_kernel_lamda_mu_to_dmodel
       print *, '[ERROR]: use_kernel_mask must be 0 or 1'
       stop
   end select
+
+  !===== parse model tags
+
+  call sem_utils_delimit_string(model_tags, ',', model_names, nmodel)
+
+  print *, '# nmodel=', nmodel
+  print *, '# model_names=', (trim(model_names(i))//"  ", i=1,nmodel)
 
   !===== create dmodel from kernel (steepest descent)
 
@@ -104,9 +118,21 @@ program xsem_kernel_lamda_mu_to_dmodel
     endif
 
     ! read kernels
-    call sem_io_read_gll_file_1(kernel_dir, iproc, iregion, 'lamda_kernel', dlamda)
-    call sem_io_read_gll_file_1(kernel_dir, iproc, iregion, 'mu_kernel', dmu)
-    call sem_io_read_gll_file_1(kernel_dir, iproc, iregion, 'rho_kernel', drho)
+    dlamda = 0.0_dp
+    dmu = 0.0_dp
+    drho = 0.0_dp
+    do i = 1, nmodel
+      select case (model_names(i))
+        case ('lamda_kernel')
+          call sem_io_read_gll_file_1(kernel_dir, iproc, iregion, 'lamda_kernel', dlamda)
+        case ('mu_kernel')
+          call sem_io_read_gll_file_1(kernel_dir, iproc, iregion, 'mu_kernel', dmu)
+        case ('rho_kernel')
+          call sem_io_read_gll_file_1(kernel_dir, iproc, iregion, 'rho_kernel', drho)
+        case DEFAULT
+          print *, '[WARNING] invalid model name: ', model_names(i)
+      end select
+    enddo
 
     ! apply kernel mask
     if (use_kernel_mask) then

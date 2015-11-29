354e56779e518f3add50d977d5af13292f148791
ktao <taotaokai@gmail.com>
2015-11-28 21:39:42 -0600

    update xem_pdf.f90

--- src/program/xsem_pdf.f90
+++ src/program/xsem_pdf.f90
@@ -8,8 +8,7 @@ subroutine selfdoc()
   print '(a)', "SYNOPSIS"
   print '(a)', ""
   print '(a)', "  xsem_statis \"
-  print '(a)', "    <nproc> <mesh_dir>"
-  print '(a)', "    <model_dir> <model_name> "
+  print '(a)', "    <nproc> <mesh_dir> <model_dir> <model_name> <nbin> <out_file>"
   print '(a)', ""
   print '(a)', "DESCRIPTION"
   print '(a)', ""
@@ -21,11 +20,12 @@ subroutine selfdoc()
   print '(a)', "  (string) model_dir:  directory holds proc*_reg1_<model_name>.bin"
   print '(a)', "  (string) model_name:  model name, e.g. mu_kernel "
   print '(a)', "  (int) nbin:  number of bins (-max|z|, max|z|)"
+  print '(a)', "  (string) out_file:  output file name "
   print '(a)', ""
   print '(a)', "NOTE"
   print '(a)', ""
   print '(a)', "  1. can be run in parallel"
-  print '(a)', "  2. output is a list of <bin_left> <bin_right> <pdf>"
+  print '(a)', "  2. output is a list of <z0> <z1> <percentage_of_volumetric_amplitude_integral>"
 
 end subroutine
 
@@ -44,16 +44,17 @@ program xsem_pdf
   !===== declare variables
 
   ! command line args
-  integer, parameter :: nargs = 5
+  integer, parameter :: nargs = 6
   character(len=MAX_STRING_LEN) :: args(nargs)
   integer :: nproc
   character(len=MAX_STRING_LEN) :: mesh_dir
   character(len=MAX_STRING_LEN) :: model_dir, model_name
   integer :: nbin
+  character(len=MAX_STRING_LEN) :: out_file
 
   ! local variables
   integer, parameter :: iregion = IREGION_CRUST_MANTLE ! crust_mantle
-  integer :: i, iproc
+  integer :: i, iproc, ier
   ! mpi
   integer :: myrank, nrank
   ! mesh
@@ -66,6 +67,7 @@ program xsem_pdf
   real(dp) :: zmin, zmax, min_i, max_i, bin_size
   real(dp) :: zmin_all, zmax_all
   real(dp), allocatable :: z(:), pdf(:), pdf_all(:)
+  real(dp) :: pdf_all_sum, volume, volume_sum
 
   !===== start MPI
 
@@ -93,6 +95,7 @@ program xsem_pdf
   read(args(3), '(a)') model_dir
   read(args(4), '(a)') model_name
   read(args(5), *) nbin
+  read(args(6), '(a)') out_file
 
   !===== loop each mesh/model slice
 
@@ -140,7 +143,7 @@ program xsem_pdf
   bin_size = 2.0 * zmax / nbin
   z = (/(-zmax + i*bin_size, i=0,nbin)/)
   pdf = 0.0_dp
-
+  volume = 0.0_dp
   do iproc = myrank, (nproc-1), nrank
     ! read model
     call sem_io_read_gll_file_1(model_dir, iproc, iregion, model_name, model)
@@ -152,25 +155,51 @@ program xsem_pdf
       pdf(i) = pdf(i) + sum(gll_volume*abs(model), &
         mask=(model>=z(i-1) .and. model<z(i)) )
     enddo
+    ! get volume
+    volume = volume + sum(gll_volume)
   enddo
   
   call synchronize_all()
 
   call sum_all_dp_n(pdf, pdf_all, nbin)
+  call sum_all_dp(volume, volume_sum)
 
   call synchronize_all()
 
   ! normalize PDF
   if (myrank == 0) then
-    print *, "# Integral(abs(model),dV)=", sum(pdf_all)
-    pdf_all = pdf_all / sum(pdf_all)
+    pdf_all_sum = sum(pdf_all)
+    pdf_all = pdf_all / pdf_all_sum
   endif
 
-  ! print out results
+  ! write out results
   if (myrank == 0) then
+    open(IOUT, file=trim(out_file), status='unknown', &
+      form='formatted', action='write', iostat=ier)
+
+    if (ier /= 0) then
+      write(*,*) '[ERROR] xsem_pdf: failed to open file ', trim(out_file)
+      call abort_mpi() 
+    endif
+
+    write(IOUT, '(a)') "# PDF of gll model"
+    write(IOUT, '(a,2X,I4)') "# nproc= ", nproc
+    write(IOUT, '(a,2X,a)') "# mesh_dir= ", trim(mesh_dir)
+    write(IOUT, '(a,2X,a)') "# model_dir= ", trim(model_dir)
+    write(IOUT, '(a,2X,a)') "# model_name= ", trim(model_name)
+    write(IOUT, '(a,2X,I4)') "# nbin= ", nbin
+    write(IOUT, '(a,2X,E12.4)') "# min(z)= ", zmin_all 
+    write(IOUT, '(a,2X,E12.4)') "# max(z)= ", zmax_all 
+    write(IOUT, '(a,2X,E12.4)') "# volume= ", volume 
+    write(IOUT, '(a,2X,E12.4)') "# volumetric_mean(|z|)= ", pdf_all_sum/volume
+    write(IOUT, '(a)') "# <z0> <z1> <percentage>"
+
     do i = 1, nbin
-      print '(E12.4, E12.4, E12.4)', z(i-1), z(i), pdf_all(i)
+      write(IOUT,'(E12.4, E12.4, E12.4)') z(i-1), z(i), pdf_all(i)
     enddo
+
+    close(IOUT)
+
   endif
 
   !====== exit MPI

d861fee3932d75c6ea6b360b8d977f7958192ef1
ktao <taotaokai@gmail.com>
2015-11-28 18:30:11 -0600

    bug fix, update xsem_thresholding

--- src/program/xsem_thresholding.f90
+++ src/program/xsem_thresholding.f90
@@ -20,7 +20,7 @@ subroutine selfdoc()
   print '(a)', "  (string) mesh_dir:  directory containing proc000***_reg1_solver_data.bin"
   print '(a)', "  (string) model_dir:  directory holds proc*_reg1_<model_name>.bin"
   print '(a)', "  (string) model_name:  model name, e.g. mu_kernel "
-  print '(a)', "  (float) zc: corner amplitude, weight model values z>zc"
+  print '(a)', "  (float) zc: corner amplitude, weight model values abs(z) >zc"
   print '(a)', "  (float) rmax: maximum excess ratio after thresholding, between 0 and 1"
   print '(a)', "  (string) out_dir:  output directory "
   print '(a)', "  (string) out_name:  output model name "
@@ -33,7 +33,7 @@ end subroutine
 
 
 !///////////////////////////////////////////////////////////////////////////////
-program xsem_statis
+program xsem_thresholding
   
   use sem_constants
   use sem_io
@@ -62,13 +62,12 @@ program xsem_statis
   ! model
   real(dp), allocatable :: model(:,:,:,:)
   ! thresholding 
-  real(dp) :: weight
 
   !===== read command line arguments
 
   if (command_argument_count() /= nargs) then
     call selfdoc()
-    print *, "[ERROR] xsem_math: check your input arguments."
+    print *, "[ERROR] xsem_thresholding: check your input arguments."
     stop
   endif
 
@@ -79,7 +78,7 @@ program xsem_statis
   read(args(2), '(a)') mesh_dir
   read(args(3), '(a)') model_dir
   read(args(4), '(a)') model_name
-  read(args(5), *) zc 
+  read(args(5), *) zc
   read(args(6), *) rmax
   read(args(7), '(a)') out_dir
   read(args(8), '(a)') out_name
@@ -102,7 +101,7 @@ program xsem_statis
     call sem_io_read_gll_file_1(model_dir, iproc, iregion, model_name, model)
 
     ! thresholding
-    where (abs(model)>zc) model = model * (1+rmax)*zc / (model + rmax*zc)
+    where (abs(model)>zc) model = model * (1+rmax)*zc / (abs(model) + rmax*zc)
 
     ! write out
     call sem_io_write_gll_file_1(out_dir, iproc, iregion, out_name, model)

8d611681e3aa5a83b3812f210b71fa963da35a0d
ktao <taotaokai@gmail.com>
2015-12-06 21:57:54 -0600

    update utils

--- utils/qsub_iteration_jobs.sh
+++ utils/qsub_iteration_jobs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # submit job scripts for one iteration:
-#   - model update
-#   - mesh
+#   - update_model + mesh
 #   - forward+adjoint for each event 
-#   - model gradient, preconditioner, direction 
+#   - update_kernel 
 
-control_file=${1:?must provide control_file}
-event_list=${2:?must provide event_id_list}
+control_file=${1:?[arg] need control_file}
+event_list=${2:?[arg] need event_list}
 
 # check inputs
 if [ ! -f "$control_file" ]
@@ -29,7 +28,7 @@ source ${control_file}
 
 #====== iteration
 echo
-echo "====== iteration: ${iter}"
+echo "#====== iteration: ${iter}"
 echo
 
 if [ ! -d ${iter_dir} ];then
@@ -49,7 +48,7 @@ ln -sf $iter_dir
 
 #====== mesh
 echo
-echo ====== mesh: submit job script
+echo "#====== mesh: submit job script"
 echo
 
 cd $base_dir
@@ -63,7 +62,7 @@ cat <<EOF > ${job_file}
 #$ -o ${iter_dir}/mesh.${iter}.o\$JOB_ID    # Name of the output file (eg. myMPI.oJobID)
 #$ -pe 12way $nproc_request                 # Requests 12 cores/node, 24 cores total: 12way 24
 #$ -q normal                                # Queue name
-#$ -l h_rt=$run_time_mesher                 # Run time (hh:mm:ss) - 1.5 hours
+#$ -l h_rt=$run_time_mesh                   # Run time (hh:mm:ss) - 1.5 hours
 #$ -M kai.tao@utexas.edu                    # email 
 #$ -m bea                                   # email info: begin/end/abort
 #$ -hold_jid -1                             # dependent job id
@@ -122,7 +121,7 @@ cat<<EOF > $job_file
 #!/bin/bash
 #$ -V                                       # Inherit the submission environment 
 #$ -cwd                                     # Start job in submission directory
-#$ -N $evid.$iter                          # Job Name
+#$ -N $evid.$iter                           # Job Name
 #$ -j y                                     # combine stderr & stdout into stdout  
 #$ -o ${iter_dir}/${evid}.${iter}.o\$JOB_ID # Name of the output file (eg. myMPI.oJobID)
 #$ -pe 12way $nproc_request                 # Requests 12 cores/node, 24 cores total: 12way 24
@@ -139,19 +138,19 @@ echo "Job begins: JOB_ID=\${JOB_ID} [\$(date)]"
 echo
 
 echo
-echo "====== forward [\$(date)]"
+echo "#====== forward [\$(date)]"
 echo
 $sem_utils/utils/setup_event.sh $control_file $evid
 cd $iter_dir/$evid
 ibrun $build_dir/bin/xspecfem3D
 
 echo
-echo "====== measure misfit [\$(date)]"
+echo "#====== measure misfit [\$(date)]"
 echo
 $sem_utils/utils/measure_adjoint.sh $control_file $evid
 
 echo
-echo "====== adjoint(model) [\$(date)]"
+echo "#====== adjoint(model) [\$(date)]"
 echo
 $sem_utils/utils/setup_adjoint.sh $control_file $evid
 cd $iter_dir/$evid

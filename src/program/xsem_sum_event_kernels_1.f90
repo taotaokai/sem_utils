bdc3f29934734a4e41433eb72c041148322ba30c
ktao <taotaokai@gmail.com>
2015-12-12 22:03:19 -0600

    update xsem_sum_event_kernels_...

--- src/program/xsem_sum_event_kernels_1.f90
+++ src/program/xsem_sum_event_kernels_1.f90
@@ -8,8 +8,8 @@ subroutine selfdoc()
   print '(a)', "SYNOPSIS"
   print '(a)', ""
   print '(a)', "  xsem_sum_event_kernels_1 \"
-  print '(a)', "    <nproc> <mesh_dir> <kernel_dir_list> <kernel_name> <use_mask>"
-  print '(a)', "    <nroot> <out_dir>"
+  print '(a)', "    <nproc> <mesh_dir> <kernel_dir_list> <kernel_name> "
+  print '(a)', "    <use_mask> <mask_tag> <out_dir>"
   print '(a)', ""
   print '(a)', "DESCRIPTION"
   print '(a)', ""
@@ -22,12 +22,14 @@ subroutine selfdoc()
   print '(a)', "  (string) kernel_dir_list:  list of event kernel directories (proc***_reg1_***_kernel.bin)"
   print '(a)', "  (string) kernel_name:  kernel name to be summed (e.g. rho_kernel)"
   print '(a)', "  (int) use_mask:  flag whether apply kernel mask (mask file is read from each kernel_dir)"
-  print '(a)', "  (int) nroot:  n-th root stacking (must be positive integer)"
+  print '(a)', "  (string) mask_tag:  tags of mask files"
   print '(a)', "  (string) out_dir:  directory to write out summed kernel files"
   print '(a)', ""
   print '(a)', "NOTE"
   print '(a)', ""
   print '(a)', "  1. can be run in parallel"
+  print '(a)', "  2. mask files: <kernel_dir>/proc***_reg1_<mask_tag>.bin"
+  print '(a)', ""
 
 end subroutine
 
@@ -53,7 +55,7 @@ program xsem_sum_event_kernels_cijkl
   character(len=MAX_STRING_LEN) :: kernel_dir_list
   character(len=MAX_STRING_LEN) :: kernel_name
   logical :: use_mask
-  integer :: nroot
+  character(len=MAX_STRING_LEN) :: mask_tag 
   character(len=MAX_STRING_LEN) :: out_dir
 
   ! local variables
@@ -72,10 +74,6 @@ program xsem_sum_event_kernels_cijkl
   ! kernel gll 
   real(dp), allocatable :: kernel(:,:,:,:)
   real(dp), allocatable :: kernel_sum(:,:,:,:)
-  ! n-th root stacking
-  real(dp) :: one_over_nroot
-  real(dp), allocatable :: sign_kernel(:,:,:,:)
-  real(dp), allocatable :: ones_kernel(:,:,:,:)
 
   !===== start MPI
 
@@ -111,14 +109,8 @@ program xsem_sum_event_kernels_cijkl
         call abort_mpi()
       endif
   end select
-  read(args(6), *) nroot
-  if (nroot < 1) then
-    if (myrank==0) then
-      print *, '[ERROR]: <nroot> must g.e. 1'
-      call abort_mpi()
-    endif
-  endif
-  read(args(7), '(a)') out_dir 
+  read(args(6), '(a)') mask_tag 
+  read(args(7), '(a)') out_dir
 
   call synchronize_all()
 
@@ -140,14 +132,9 @@ program xsem_sum_event_kernels_cijkl
   allocate(kernel(NGLLX,NGLLY,NGLLZ,nspec))
   allocate(kernel_sum(NGLLX,NGLLY,NGLLZ,nspec))
   allocate(mask(NGLLX,NGLLY,NGLLZ,nspec))
-  allocate(sign_kernel(NGLLX,NGLLY,NGLLZ,nspec))
-  allocate(ones_kernel(NGLLX,NGLLY,NGLLZ,nspec))
 
   ! combine event kernels
-  one_over_nroot = 1.0_dp / nroot
   mask = 1.0_dp
-  sign_kernel = 1.0_dp
-  ones_kernel = 1.0_dp
   do iproc = myrank, (nproc-1), nrank
 
     print *, '#-- iproc=', iproc
@@ -162,19 +149,13 @@ program xsem_sum_event_kernels_cijkl
       ! read mask
       if (use_mask) then
         call sem_io_read_gll_file_1(kernel_dirs(iker), iproc, iregion, &
-          "mask", mask)
+          mask_tag, mask)
       endif
 
-      ! n-th root stacking
-      sign_kernel = sign(ones_kernel, kernel)
-      kernel_sum = kernel_sum &
-        + sign_kernel * abs(kernel)**one_over_nroot * mask
+      ! sum up kernels
+      kernel_sum = kernel_sum + kernel * mask
 
     enddo ! iker
-
-    ! n-th root stacking
-    sign_kernel = sign(ones_kernel, kernel_sum)
-    kernel_sum = sign_kernel * abs(kernel_sum)**nroot
 
     ! write out gll file
     call sem_io_write_gll_file_1(out_dir, iproc, iregion, &

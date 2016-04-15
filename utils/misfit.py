aaa96e6e6b9fdbf8dcffe67f992c4c3d39c4545b
ktao <taotaokai@gmail.com>
2016-04-15 12:28:00 -0500

    update utils/misfit.py

--- utils/misfit.py
+++ utils/misfit.py
@@ -2046,7 +2046,8 @@ class Misfit(object):
         },
       axes=[ ('t0','xs'), ('t0',), ('xs',) ],
       outfig="grid_cc.pdf",
-      plot_seism=False
+      plot_seism=False,
+      cmt_file="CMTSOLUTION.grid_cc"
       ):
     """ calculate misfit over 2D model grids based on perturbed seismograms 
     """
@@ -2166,6 +2167,41 @@ class Misfit(object):
           'alpha':alpha,
           'm1':m0+alpha*dm
           }
+
+    #------ write out new CMTSOLUTION file
+    t0 = event['t0']
+    if 't0' in model: t0 = model['t0']['m1']
+    # modify origin time in header line to have centroid time 
+    header = event['header'].split()
+    header[1] = str(t0.year)
+    header[2] = str(t0.month)
+    header[3] = str(t0.day)
+    header[4] = str(t0.hour)
+    header[5] = str(t0.minute)
+    header[6] = str(t0.second + 1.0e-6*t0.microsecond)
+
+    tau = event['tau']
+    if 'tau' in model: tau = model['tau']['m1']
+    xs = event['xs']
+    if 'xs' in model: xs = model['xs']['m1']
+    mt = event['mt']
+    if 'mt' in model: mt = model['mt']['m1']
+
+    with open(cmt_file, 'w') as fp:
+      fp.write('%s | wCCmax %f weight %f\n'
+          % (' '.join(header), zz_max, weight))
+      fp.write('%-18s %s\n' % ('event name:', "grid_cc"))
+      fp.write('%-18s %+15.8E\n' % ('t0(s):',    0.0))
+      fp.write('%-18s %+15.8E\n' % ('tau(s):',   tau))
+      fp.write('%-18s %+15.8E\n' % ('x(m):',     xs[0]))
+      fp.write('%-18s %+15.8E\n' % ('y(m):',     xs[1]))
+      fp.write('%-18s %+15.8E\n' % ('z(m):',     xs[2]))
+      fp.write('%-18s %+15.8E\n' % ('Mxx(N*m):', mt[0,0]))
+      fp.write('%-18s %+15.8E\n' % ('Myy(N*m):', mt[1,1]))
+      fp.write('%-18s %+15.8E\n' % ('Mzz(N*m):', mt[2,2]))
+      fp.write('%-18s %+15.8E\n' % ('Mxy(N*m):', mt[0,1]))
+      fp.write('%-18s %+15.8E\n' % ('Mxz(N*m):', mt[0,2]))
+      fp.write('%-18s %+15.8E\n' % ('Myz(N*m):', mt[1,2]))
 
     return model, zz_max, weight
 

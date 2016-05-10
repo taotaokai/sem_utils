b31ad9341d13b9545891320d03ea560312e04fdc
Kai Tao <ktao@login2.ls5.tacc.utexas.edu>
2016-05-09 22:36:44 -0500

    update misfit_v0 from ls5

---
+++ utils/misfit_v0/output_misfit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Process misfit
+"""
+import sys
+from misfit import Misfit
+
+# read command line args
+misfit_file = str(sys.argv[1])
+out_file = str(sys.argv[2])
+
+print("\n====== initialize\n")
+misfit = Misfit()
+
+print("\n====== load data\n")
+misfit.load(filename=misfit_file)
+
+print("\n====== output misfit measurments\n")
+misfit.output_misfit(out_file)

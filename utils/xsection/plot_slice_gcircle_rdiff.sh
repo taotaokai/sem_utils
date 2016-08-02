f4cae24a75a2e4e796539531d62093cc1eb5088b
ktao <taotaokai@gmail.com>
2016-08-01 20:13:24 -0500

    add utils/xsection/plot_slice_gcircle_rdiff.py/sh

---
+++ utils/xsection/plot_slice_gcircle_rdiff.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# plot xsections for model relative difference
+sem_utils=~/seiscode/sem_utils
+
+ref_model_dir=${1:?[arg]need ref_model_dir (*.nc)}
+new_model_dir=${2:?[arg]need new_model_dir (*.nc)}
+slice_list=${3:?[arg]need slice_list}
+out_dir=${4:?[arg]need out_dir}
+title=${5:?[arg]need title}
+
+awk 'NF&&$1!~/#/' $slice_list |\
+while read lat0 lon0 azimuth theta0 theta1 ntheta r0 r1 nr nc_tag 
+do
+  echo
+  echo "#====== $nc_tag: $lat0 $lon0 $azimuth $theta0 $theta1 $ntheta $r0 $r1 $nr"
+  echo
+  $sem_utils/utils/xsection/plot_slice_gcircle_rdiff.py \
+    $ref_model_dir \
+    $new_model_dir \
+    $nc_tag \
+    $lat0 $lon0 $azimuth \
+    "$title" $out_dir
+done

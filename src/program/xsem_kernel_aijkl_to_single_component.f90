8d749865a93a1f3c16572cd3882fe87e995f4256
ktao <taotaokai@gmail.com>
2017-07-03 17:09:25 +0800

    add xsem_kernel_aijkl_to_single_component

---
+++ src/program/xsem_kernel_aijkl_to_single_component.f90
@@ -0,0 +1,128 @@
+subroutine selfdoc()
+
+  print '(a)', "NAME"
+  print '(a)', ""
+  print '(a)', "  xsem_kernel_aijkl_to_iso"
+  print '(a)', "    - extract each component from aijkl kernel"
+  print '(a)', ""
+  print '(a)', "SYNOPSIS"
+  print '(a)', ""
+  print '(a)', "  xsem_kernel_aijkl_to_iso \"
+  print '(a)', "    <nproc> <mesh_dir> <kernel_dir> <out_dir>"
+  print '(a)', ""
+  print '(a)', "DESCRIPTION"
+  print '(a)', ""
+  print '(a)', "  aijkl kernels are indexed from 1 to 21 as defined in "
+  print '(a)', "  specfem3d_globe/src/specfem3D/compute_kernels.f90:compute_strain_product()"
+  print '(a)', ""
+  print '(a)', "PARAMETERS"
+  print '(a)', ""
+  print '(a)', "  (int) nproc:  number of mesh slices"
+  print '(a)', "  (string) mesh_dir:  directory holds proc*_reg1_solver_data.bin"
+  print '(a)', "  (string) kernel_dir:  directory holds the aijkl_kernel files"
+  print '(a)', "                        proc******_reg1_aijkl_kernel.bin"
+  print '(a)', "  (string) out_dir:  output directory for [a1,a2,...]_kernel.bin"
+  print '(a)', ""
+  print '(a)', "NOTE"
+  print '(a)', ""
+  print '(a)', "  1. can be run in parallel"
+
+end subroutine
+
+
+!///////////////////////////////////////////////////////////////////////////////
+program xsem_kernel_aijkl_to_iso
+
+  use sem_constants
+  use sem_io
+  use sem_mesh
+  use sem_utils
+  use sem_parallel
+
+  implicit none
+
+  !===== declare variables
+  ! command line args
+  integer, parameter :: nargs = 4
+  character(len=MAX_STRING_LEN) :: args(nargs)
+  integer :: nproc
+  character(len=MAX_STRING_LEN) :: mesh_dir
+  character(len=MAX_STRING_LEN) :: kernel_dir
+  character(len=MAX_STRING_LEN) :: out_dir
+
+  ! local variables
+  integer, parameter :: iregion = IREGION_CRUST_MANTLE ! crust_mantle
+  integer :: i, iproc
+
+  ! mpi
+  integer :: myrank, nrank
+
+  ! mesh
+  type(sem_mesh_data) :: mesh_data
+  integer :: nspec
+
+  ! kernel gll 
+  real(dp), allocatable :: aijkl_kernel(:,:,:,:,:)
+  real(dp), allocatable :: kernel(:,:,:,:)
+  character(len=MAX_STRING_LEN) :: kernel_name
+
+  !===== start MPI
+
+  call init_mpi()
+  call world_size(nrank)
+  call world_rank(myrank)
+
+  !===== read command line arguments
+  if (command_argument_count() /= nargs) then
+    if (myrank == 0) then
+      call selfdoc()
+      print *, "[ERROR] xsem_kernel_aijkl_to_iso: check your inputs."
+      call abort_mpi()
+    endif 
+  endif
+  call synchronize_all()
+
+  do i = 1, nargs
+    call get_command_argument(i, args(i))
+  enddo
+  read(args(1), *) nproc
+  read(args(2), '(a)') mesh_dir
+  read(args(3), '(a)') kernel_dir
+  read(args(4), '(a)') out_dir 
+
+  !====== loop model slices 
+
+  ! get mesh geometry
+  if (myrank == 0) then
+    call sem_mesh_read(mesh_dir, myrank, iregion, mesh_data)
+    nspec = mesh_data%nspec
+  endif
+  call bcast_all_singlei(nspec)
+
+  call synchronize_all()
+
+  ! initialize gll arrays 
+  allocate(aijkl_kernel(21,NGLLX,NGLLY,NGLLZ,nspec))
+
+  ! reduce cijkl kernels
+  do iproc = myrank, (nproc-1), nrank
+
+    print *, '# iproc=', iproc
+
+    ! read cijkl kernel gll file
+    call sem_io_read_cijkl_kernel(kernel_dir, iproc, iregion, 'aijkl_kernel', aijkl_kernel)
+
+    ! write out a,b kernel
+    do i = 1, 21
+      write (kernel_name, "('a',I0.2,'_kernel')") i
+      print *, trim(kernel_name)
+      call sem_io_write_gll_file_1(out_dir, iproc, iregion, kernel_name, aijkl_kernel(i,:,:,:,:))
+    enddo
+
+  enddo ! iproc
+
+  !====== exit MPI
+  call synchronize_all()
+  call finalize_mpi()
+
+end program

6345f1c91c733057f86d2c73aade414fc994c1c9
Kai Tao <taotaokai@gmail.com>
2025-05-14 00:00:43 +0800

    update

---
+++ utils/misfit_dev_h5/grid_search_source.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""calculate cc for step sizes
+"""
+import sys
+import argparse
+from datetime import datetime
+from misfit import Misfit
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("misfit_h5file" )  # "misfit.h5"
+parser.add_argument("out_txt")  # "search.txt"
+parser.add_argument("out_fig")  # "search.pdf"
+
+args = parser.parse_args()
+
+with Misfit(args.misfit_h5file, 'r') as misfit:
+    misfit.grid_search_source(args.out_txt, args.out_fig)

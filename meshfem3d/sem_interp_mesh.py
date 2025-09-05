9a7f3394d2e90054dcd958f9641bde6dced51994
Kai Tao <taotaokai@gmail.com>
2025-09-05 14:20:21 +0800

    update

--- meshfem3d/sem_interp_mesh.py
+++ meshfem3d/sem_interp_mesh.py
@@ -8,10 +8,9 @@ import argparse
 
 import numpy as np
 from scipy.io import FortranFile
-
 from mpi4py import MPI
 
-from meshfem3d_constants import NGLLX, NGLLY, NGLLZ, GAUSSALPHA, GAUSSBETA
+# from meshfem3d_constants import NGLLX, NGLLY, NGLLZ, GAUSSALPHA, GAUSSBETA
 from meshfem3d_constants import (
     IFLAG_CRUST,
     IFLAG_80_MOHO,
@@ -20,22 +19,42 @@ from meshfem3d_constants import (
     IFLAG_DUMMY,
 )
 
-from meshfem3d_utils import get_gll_weights, sem_mesh_read, sem_mesh_locate_points
+from meshfem3d_utils import (
+    get_gll_weights,
+    lagrange_poly,
+    sem_mesh_read,
+    sem_mesh_locate_points,
+)
 
 # ====== parameters
 
-# mesh files to interpolate from
-nproc_source = int(sys.argv[1])
-mesh_dir_source = str(sys.argv[2])  # <mesh_dir>/proc******_external_mesh.bin
-model_dir_source = str(sys.argv[3])  # <model_dir>/proc******_<model_name>.bin
+parser = argparse.ArgumentParser()
 
-# mesh files for which to get interpolated values
-nproc_target = int(sys.argv[4])
-mesh_dir_target = str(sys.argv[5])  # <mesh_dir>/proc******_external_mesh.bin
-# model_dir_target = str(sys.argv[6]) # <model_dir>/proc******_<model_name>.bin
+parser.add_argument("nproc_source", help="input mesh nproc", type=int)
+parser.add_argument("mesh_dir_source", help="input mesh dir")
+parser.add_argument("model_dir_source", help="input model dir")
 
-model_names = str(sys.argv[6])  # comma delimited e.g. vp,vs,rho,qmu,qkappa
-out_dir = str(sys.argv[7])
+parser.add_argument("nproc_target", help="nproc", type=int)
+parser.add_argument("mesh_dir_target", help="mesh dir for interpolate")
+parser.add_argument("model_dir_target", help="output model dir")
+
+parser.add_argument(
+    "model_tags", nargs="+", help="model tags to interpolate (e.g. vsv vsh)"
+)
+
+args = parser.parse_args()
+print(args)
+
+nproc_source = args.nproc_source
+mesh_dir_source = args.mesh_dir_source
+model_dir_source = args.model_dir_source
+
+nproc_target = args.nproc_target
+mesh_dir_target = args.mesh_dir_target
+model_dir_target = args.model_dir_target
+
+model_tags = args.model_tags
+nmodel = len(model_tags)
 
 # merge regions
 idoubling_merge = []
@@ -43,25 +62,19 @@ idoubling_merge = []
 # idoubling_merge = [IFLAG_CRUST, IFLAG_80_MOHO, IFLAG_220_80, IFLAG_670_220]
 # idoubling_merge = [IFLAG_CRUST, IFLAG_80_MOHO, IFLAG_220_80]
 
-# model names
-model_names = model_names.split(",")
-nmodel = len(model_names)
-
 # ====== interpolate
 comm = MPI.COMM_WORLD
 mpi_size = comm.Get_size()
 mpi_rank = comm.Get_rank()
 
-# GLL
-xigll, wx = zwgljd(NGLLX, GAUSSALPHA, GAUSSBETA)
-yigll, wy = zwgljd(NGLLY, GAUSSALPHA, GAUSSBETA)
-zigll, wz = zwgljd(NGLLZ, GAUSSALPHA, GAUSSBETA)
+# GLL nodes
+zgll, wgll, dlag_dzgll = get_gll_weights()
 
 # --- loop over each slice of target SEM mesh
+#for iproc_target in range(nproc_target):
 for iproc_target in range(mpi_rank, nproc_target, mpi_size):
-
-    print("====== iproc_target ", iproc_target)
-    sys.stdout.flush()
+    # print("====== iproc_target ", iproc_target)
+    # sys.stdout.flush()
 
     # read in target SEM mesh
     mesh_file = "%s/proc%06d_reg1_solver_data.bin" % (mesh_dir_target, iproc_target)
@@ -70,6 +83,7 @@ for iproc_target in range(mpi_rank, nproc_target, mpi_size):
     ibool_target = mesh_data_target["ibool"]
     idoubling_target = mesh_data_target["idoubling"]
     xyz_glob_target = mesh_data_target["xyz_glob"]
+    # print(xyz_glob_target.shape)
 
     # merge regions if required
     idx_merge = np.zeros(nspec_target, dtype="bool")
@@ -78,24 +92,24 @@ for iproc_target in range(mpi_rank, nproc_target, mpi_size):
     idoubling_target[idx_merge] = IFLAG_DUMMY
 
     # xyz points to locate
-    xyz_target = xyz_glob_target[:, ibool_target.ravel() - 1]
-    idoubling_ext = np.zeros(ibool_target.shape, dtype="int") + idoubling_target
-    idoubling_ext = idoubling_ext.ravel()
+    # xyz_target = xyz_glob_target[ibool_target.ravle(), :]
+    # idoubling_ext = np.zeros(ibool_target.shape, dtype="int") + idoubling_target
+    # idoubling_ext = idoubling_ext.ravel()
+    xyz_target = xyz_glob_target
+    idoubling_ext = -1
 
     # array of final results
-    npoints = xyz_target.shape[1]
-    status_gll_target = np.zeros(npoints, dtype="int")
-    status_gll_target[:] = -1
-    misloc_gll_target = np.zeros(npoints)
-    misloc_gll_target[:] = np.inf
-    misratio_gll_target = np.zeros(npoints)
-    model_gll_target = np.zeros((nmodel, npoints))
+    npoints = xyz_target.shape[0]
+    status_target = np.zeros(npoints, dtype="int")
+    status_target[:] = -1
+    misloc_target = np.zeros(npoints)
+    misloc_target[:] = np.inf
+    misratio_target = np.zeros(npoints)
+    model_target = np.zeros((nmodel, npoints))
 
-    # loop over each slice of source SEM mesh
+    # -- loop over each slice of source SEM mesh
     for iproc_source in range(nproc_source):
-
-        print("iproc_target / iproc_source ", iproc_target, iproc_source)
-        sys.stdout.flush()
+        tic = time.time()
 
         # read in source SEM mesh
         mesh_file = "%s/proc%06d_reg1_solver_data.bin" % (mesh_dir_source, iproc_source)
@@ -112,21 +126,22 @@ for iproc_target in range(mpi_rank, nproc_target, mpi_size):
         gll_dims = mesh_data_source["gll_dims"]
         source_model_gll = np.zeros((nmodel,) + gll_dims)
         for imodel in range(nmodel):
-            model_tag = model_names[imodel]
+            model_tag = model_tags[imodel]
             model_file = "%s/proc%06d_reg1_%s.bin" % (
                 model_dir_source,
                 iproc_source,
                 model_tag,
             )
             with FortranFile(model_file, "r") as f:
-                # note: must use fortran convention when reshape to N-D array!!!
+                # here gll_dims = [NSPEC, NGLLZ, NGLLY, NGLLX] which is C convention of
+                # Fortran array of [NGLLX, NGLLY, NGLLZ, NSPEC]
                 source_model_gll[imodel, :, :, :, :] = np.reshape(
-                    f.read_ints(dtype="f4"), gll_dims, order="F"
+                    f.read_ints(dtype="f4"), gll_dims
                 )
 
         # locate target points
         status_all, ispec_all, uvw_all, misloc_all, misratio_all = (
-            sem_locate_points_hex27(
+            sem_mesh_locate_points(
                 mesh_data_source,
                 xyz_target,
                 idoubling_ext,
@@ -138,28 +153,19 @@ for iproc_target in range(mpi_rank, nproc_target, mpi_size):
         # merge interpolation results of mesh slice (iproc_souce) into
         # the final results based on misloc and status
 
-        # index selection for merge: (not located inside an element yet) and (located for the current mesh slice) and ( smaller misloc or located inside an element in this mesh slice )
+        # index selection for merge: 
+        # (not located inside an element yet) and 
+        # (located for the current mesh slice) and 
+        # ( smaller misloc or located inside an element in this mesh slice )
         ii = (
-            (status_gll_target != 1)
+            (status_target != 1)
             & (status_all != -1)
-            & ((misloc_all < misloc_gll_target) | (status_all == 1))
+            & ((misloc_all < misloc_target) | (status_all == 1))
         )
 
-        status_gll_target[ii] = status_all[ii]
-        misloc_gll_target[ii] = misloc_all[ii]
-        misratio_gll_target[ii] = misratio_all[ii]
-
-        # hlagx = lagrange_poly(xigll, uvw_all[0,ii]).reshape((NGLLX,1,1,-1))
-        # hlagy = lagrange_poly(yigll, uvw_all[1,ii]).reshape((1,NGLLY,1,-1))
-        # hlagz = lagrange_poly(zigll, uvw_all[2,ii]).reshape((1,1,NGLLZ,-1))
-        # model_gll_target[:,ii] = np.sum(source_model_gll[:,:,:,:,ispec_all[ii]]*hlagx*hlagy*hlagz, axis=(1,2,3))
-
-        # FIXME replace with index slicing
-        # status_gll_target[i] = status_all[ii]
-        # ...
-        # modify lagrange_poly(xigll,x) to handle x(n)
-        # ipoint_select = np.nonzero(ii)[0]
-        # DONE
+        status_target[ii] = status_all[ii]
+        misloc_target[ii] = misloc_all[ii]
+        misratio_target[ii] = misratio_all[ii]
 
         # NOTE avoid too many for loops reduces computation time
         ##for ipoint in range(npoints):
@@ -181,48 +187,55 @@ for iproc_target in range(mpi_rank, nproc_target, mpi_size):
             # status_gll_target[ipoint] = status_all[ipoint]
             # misloc_gll_target[ipoint] = misloc_all[ipoint]
             # misratio_gll_target[ipoint] = misratio_all[ipoint]
-            hlagx = lagrange_poly(xigll, uvw_all[0, ipoint])[:, 0]
-            hlagy = lagrange_poly(yigll, uvw_all[1, ipoint])[:, 0]
-            hlagz = lagrange_poly(zigll, uvw_all[2, ipoint])[:, 0]
-            model_gll_target[:, ipoint] = np.sum(
-                source_model_gll[:, :, :, :, ispec_all[ipoint]]
-                * hlagx[:, None, None]
-                * hlagy[None, :, None]
-                * hlagz[None, None, :],
+            hlagx = lagrange_poly(zgll, uvw_all[ipoint, 0])
+            hlagy = lagrange_poly(zgll, uvw_all[ipoint, 1])
+            hlagz = lagrange_poly(zgll, uvw_all[ipoint, 2])
+            model_target[:, ipoint] = np.sum(
+                source_model_gll[:, ispec_all[ipoint], :, :, :]
+                * hlagx[None, None, None, :]
+                * hlagy[None, None, :, None]
+                * hlagz[None, :, None, None],
                 axis=(1, 2, 3),
             )
 
-    # end for loop over each slice of source SEM mesh
+        elapsed_time = time.time() - tic
+        print(f"{iproc_target=}, {iproc_source=}, {elapsed_time=}")
+        sys.stdout.flush()
 
     # rehape results
     gll_dims = mesh_data_target["gll_dims"]
-    status_gll_target = np.reshape(status_gll_target, gll_dims)
-    misloc_gll_target = np.reshape(misloc_gll_target, gll_dims)
-    misratio_gll_target = np.reshape(misratio_gll_target, gll_dims)
-    gll_dims = (nmodel,) + gll_dims
-    model_gll_target = np.reshape(model_gll_target, gll_dims)
+    # status_gll_target = np.reshape(status_gll_target, gll_dims)
+    # misloc_gll_target = np.reshape(misloc_gll_target, gll_dims)
+    # misratio_gll_target = np.reshape(misratio_gll_target, gll_dims)
+    # gll_dims = (nmodel,) + gll_dims
+    # model_gll_target = np.reshape(model_gll_target, gll_dims)
 
     # save interpolated model
     for imodel in range(nmodel):
-        model_tag = model_names[imodel]
-        model_file = "%s/proc%06d_reg1_%s.bin" % (out_dir, iproc_target, model_tag)
+        model_tag = model_tags[imodel]
+        model_file = "%s/proc%06d_reg1_%s.bin" % (
+            model_dir_target,
+            iproc_target,
+            model_tag,
+        )
+        model_gll = model_target[imodel, ibool_target]
         with FortranFile(model_file, "w") as f:
             f.write_record(
                 np.array(
-                    np.ravel(model_gll_target[imodel, :, :, :, :], order="F"),
+                    np.ravel(model_gll),
                     dtype="f4",
                 )
             )
 
     # save misloc, status
-    model_file = "%s/proc%06d_reg1_status.bin" % (out_dir, iproc_target)
+    model_file = "%s/proc%06d_reg1_status.bin" % (model_dir_target, iproc_target)
     with FortranFile(model_file, "w") as f:
-        f.write_record(np.array(np.ravel(status_gll_target, order="F"), dtype="f4"))
+        f.write_record(np.array(np.ravel(status_target[ibool_target], order="F"), dtype="f4"))
 
-    model_file = "%s/proc%06d_reg1_misloc.bin" % (out_dir, iproc_target)
+    model_file = "%s/proc%06d_reg1_misloc.bin" % (model_dir_target, iproc_target)
     with FortranFile(model_file, "w") as f:
-        f.write_record(np.array(np.ravel(misloc_gll_target, order="F"), dtype="f4"))
+        f.write_record(np.array(np.ravel(misloc_target[ibool_target], order="F"), dtype="f4"))
 
-# model_file = "%s/proc%06d_reg1_misratio.bin"%(out_dir, iproc_target)
-# with FortranFile(model_file, 'w') as f:
-#   f.write_record(np.array(np.ravel(misratio_gll_target, order='F'), dtype='f4'))
+    model_file = "%s/proc%06d_reg1_misratio.bin" % (model_dir_target, iproc_target)
+    with FortranFile(model_file, "w") as f:
+        f.write_record(np.array(np.ravel(misratio_target[ibool_target], order="F"), dtype="f4"))

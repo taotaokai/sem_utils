e71529a30cb82e03f8b22f0aad4c29f2fe69b850
Kai Tao <taotaokai@gmail.com>
2025-10-06 16:05:19 +0800

    update

--- meshfem3d/sem_truncate_value.py
+++ meshfem3d/sem_truncate_value.py
@@ -93,7 +93,6 @@ def process(nproc, mesh_dir, model_dir, model_tag, out_dir, nbins=100,
         model_gll = read_gll_file(model_dir, model_tag, iproc)
         max_amp = max(max_amp, np.max(np.abs(model_gll)))
     max_amp = mpi_comm.allreduce(max_amp, op=MPI.MAX)
-    print(f"DEBUG: {mpi_rank=}, {max_amp=}")
 
     # cumulative distribution by bins
     # bin_edges = np.linspace(0, max_amp, nbins + 1)
@@ -114,17 +113,13 @@ def process(nproc, mesh_dir, model_dir, model_tag, out_dir, nbins=100,
             # pdf_local[ibin] = np.sum(model_gll[mask] * vol_gll[mask])
             pdf_local[ibin] = np.sum(vol_gll[mask])
         pdf += pdf_local
-    print(f"DEBUG: {mpi_rank=}, {pdf=}")
     mpi_comm.Allreduce(MPI.IN_PLACE, pdf, op=MPI.SUM)
     pdf /= np.sum(pdf)  # normalize
-    print(f"DEBUG: {mpi_rank=}, {pdf=}")
     cdf = np.cumsum(pdf)
-    print(f"DEBUG: {mpi_rank=}, {cdf=}")
     ind = (cdf >= truncate_percentage).nonzero()[0][0]
     truncate_value = bin_edges[ind+1]
 
     if mpi_rank == 0:
-        print(f"Writing cdf to {cdf_file}")
         np.savetxt(cdf_file, cdf)
 
     # write out threshold gll files

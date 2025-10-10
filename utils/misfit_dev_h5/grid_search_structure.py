0a39d9eb886127bf2673c98b230a5c332fae6d9c
Kai Tao <taotaokai@gmail.com>
2025-10-10 20:54:48 +0800

    update

--- utils/misfit_dev_h5/grid_search_structure.py
+++ utils/misfit_dev_h5/grid_search_structure.py
@@ -11,10 +11,14 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("misfit_h5file" )  # "misfit.h5"
 parser.add_argument("-n", "--nproc", default=5, type=int)  # num of processes
+parser.add_argument("--par_file", default=None)  # "misfit.h5"
 
 args = parser.parse_args()
 
 misfit = Misfit(args.misfit_h5file)
+
+if args.par_file is not None:
+    misfit.read_config_file(args.par_file)
 
 print(f"======= [{datetime.now()}] grid_search_source")
 

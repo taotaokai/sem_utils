8cb5daf7dd73a2ad9e48aea4bb9da01a21ca9d95
shenhao <shenhao@mail.iggcas.ac.cn>
2025-10-17 09:04:23 +0800

    check remained events

---
+++ source_inversion_regEQ/check_events_remain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ../../
+echo "$PWD"
+list_file="event_within_mesh.lst"
+folder="./stage00.source/iter08/CMTSOLUTION_updated/"
+output_file="event_within_mesh_remain.lst"
+
+[ -f "$output_file" ] && rm "$output_file"
+
+while read -r line; do
+
+    [ -z "$line" ] && continue
+
+    filename=$(echo "$line" | awk '{print $1}')
+
+    if [ ! -e "$folder/$filename.cmt" ]; then
+        echo "$line" >> "$output_file"
+        echo "$filename does not exit in CMTSOLUTION_updated."
+    fi
+done < "$list_file"

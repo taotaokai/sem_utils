d77c9d4aa82e61bc0386852461c7e39378f9f357
Kai Tao <taotaokai@gmail.com>
2025-12-10 20:18:51 +0800

    update

--- meshfem3d/sem_make_gaussian_mask.py
+++ meshfem3d/sem_make_gaussian_mask.py
@@ -15,10 +15,11 @@ comm_world = MPI.COMM_WORLD
 size_world = comm_world.Get_size()
 rank_world = comm_world.Get_rank()
 
+
 def parse_arguments():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(
-        description="Create Gaussian mask around source/receiver locations, " \
+        description="Create Gaussian mask around source/receiver locations, "
         "prod(1 - exp(-0.5 * (x - x_i)**2 / sigma_i**2), i=0,n_mask)"
     )
 
@@ -28,11 +29,13 @@ def parse_arguments():
         "point_list",
         help="list of x,y,z[,sigma_km], where x,y,z are non-dimensionalized by R_EARTH (e.g. 6371 km), sigma_km is optional",
     )
-    parser.add_argument("--sigma_km", type=float, default=10.0, help="one sigma of Gaussian mask in km")
-    parser.add_argument("--reverse", action="store_true", help="reverse mask to 1.0 - mask")
     parser.add_argument(
-        "out_dir", help="output dir for proc*_reg1_mask.bin"
+        "--sigma_km", type=float, default=10.0, help="one sigma of Gaussian mask in km"
     )
+    parser.add_argument(
+        "--reverse", action="store_true", help="reverse mask to 1.0 - mask"
+    )
+    parser.add_argument("out_dir", help="output dir for proc*_reg1_mask.bin")
 
     return parser.parse_args()
 
@@ -53,15 +56,15 @@ def read_list(point_list, sigma_km):
     xyz_mask = np.zeros((npts, 3), dtype=np.float32)
     sigma_mask = np.zeros(npts, dtype=np.float32)
 
-    assert 'x' in df.columns
-    assert 'y' in df.columns
-    assert 'z' in df.columns
-    xyz_mask[:, 0] = df['x'].to_numpy(dtype=np.float32)
-    xyz_mask[:, 1] = df['y'].to_numpy(dtype=np.float32)
-    xyz_mask[:, 2] = df['z'].to_numpy(dtype=np.float32)
+    assert "x" in df.columns
+    assert "y" in df.columns
+    assert "z" in df.columns
+    xyz_mask[:, 0] = df["x"].to_numpy(dtype=np.float32)
+    xyz_mask[:, 1] = df["y"].to_numpy(dtype=np.float32)
+    xyz_mask[:, 2] = df["z"].to_numpy(dtype=np.float32)
 
-    if 'sigma_km' in df.columns:
-        sigma_mask = df['sigma_km'].to_numpy(dtype=np.float32)
+    if "sigma_km" in df.columns:
+        sigma_mask = df["sigma_km"].to_numpy(dtype=np.float32)
     else:
         sigma_mask[:] = sigma_km
     sigma_mask /= R_EARTH_KM
@@ -75,17 +78,23 @@ def make_gaussian_mask(xyz_glob, xyz_mask, sigma_mask):
     nmask = xyz_mask.shape[0]
     weight_mask = np.ones(nglob, dtype=np.float32)
 
-    sigma_squared = sigma_mask**2
+    inverse_two_sigma_squared = 0.5 / sigma_mask**2
 
-    for iglob in range(nglob):
-        weight = 1
-        for imask in range(nmask):
-            weight *= 1.0 - np.exp(
-                -0.5
-                * sum((xyz_glob[iglob, :] - xyz_mask[imask, :])**2)
-                / sigma_squared[imask]
-            )
-        weight_mask[iglob] = weight
+    for imask in range(nmask):
+        weight_mask *= 1.0 - np.exp(
+            -1.0 * inverse_two_sigma_squared[imask]
+            * np.sum((xyz_glob[:, :] - xyz_mask[imask, :]) ** 2, axis=-1)
+        )
+
+    # for iglob in range(nglob):
+    #     weight = 1
+    #     for imask in range(nmask):
+    #         weight *= 1.0 - np.exp(
+    #             -0.5
+    #             * sum((xyz_glob[iglob, :] - xyz_mask[imask, :])**2)
+    #             / sigma_squared[imask]
+    #         )
+    #     weight_mask[iglob] = weight
 
     return weight_mask
 
@@ -130,9 +139,7 @@ def main():
         ibool = mesh_data["ibool"]
         out_file = "%s/proc%06d_reg1_mask.bin" % (args.out_dir, iproc)
         with FortranFile(out_file, "w") as f:
-            f.write_record(
-                np.array(weight_mask[ibool], dtype="f4")
-            )
+            f.write_record(np.array(weight_mask[ibool], dtype="f4"))
 
 
 if __name__ == "__main__":

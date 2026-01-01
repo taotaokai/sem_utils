2f0b3a85225d2c20f69e56b70b54be9188db5df0
Kai Tao <taotaokai@gmail.com>
2026-01-01 15:45:00 +0800

    update

--- utils/misfit_dev_h5/test_misfit.py
+++ utils/misfit_dev_h5/test_misfit.py
@@ -1,35 +1,35 @@
 from misfit import Misfit
 
-with Misfit('misfit.h5', 'w') as misfit:
+misfit = Misfit('misfit_test.h5')
 
-    misfit.read_config_file('misfit.yaml')
+misfit.read_config_file('misfit.yaml')
 
-    misfit.read_solver_parfile('Par_file')
+misfit.read_solver_parfile('Par_file')
 
-    misfit.read_cmtsolution('CMTSOLUTION')
+misfit.read_cmtsolution('CMTSOLUTION.ecef', ECEF=True)
 
-    misfit.write_cmtsolution('C202006211907A.ecef')
+# misfit.write_cmtsolution('C202006211907A.ecef')
 
-    misfit.read_channel_file('channel.txt')
+misfit.read_channel_file('channel.txt', "STATIONS_FILTERED")
 
-    misfit.read_data_h5('data_vel.h5')
+misfit.read_data_h5('sem.h5', "STATIONS_FILTERED")
 
-    misfit.read_syn_sac('events/C202006211907A/syn')
+misfit.read_syn_sac('sac')
 
-    misfit.setup_windows()
+misfit.setup_windows()
 
-    misfit.measure_adj()
+misfit.measure_adj()
 
-    misfit.output_adj('adj/')
+# misfit.output_adj('adj/')
 
-    misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='p,P_Z_30-100sec')
-    misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='p,P_Z_10-50sec', align_time=False)
-    misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='s,S_Z_30-100sec')
-    misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='s,S_R_30-100sec')
-    misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='s,S_T_30-100sec')
-    misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='Surf_Z_30-100sec')
-    misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='Surf_R_30-100sec')
-    misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='Surf_T_30-100sec')
+# misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='p,P_Z_30-100sec')
+# misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='p,P_Z_10-50sec', align_time=False)
+# misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='s,S_Z_30-100sec')
+# misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='s,S_R_30-100sec')
+# misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='s,S_T_30-100sec')
+# misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='Surf_Z_30-100sec')
+# misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='Surf_R_30-100sec')
+# misfit.plot_seismogram_1comp(savefig=True, out_dir='fig/', win_id='Surf_T_30-100sec')
 
 # misfit = Misfit('misfit.h5', 'w')
 # misfit.read_config_file('misfit.yaml')

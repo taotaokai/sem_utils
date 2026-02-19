84a5b48ca2be9770076f0f446ad7c9d0502648d5
Kai Tao <taotaokai@gmail.com>
2026-02-19 11:57:19 +0800

    update

---
+++ utils/misfit_dev_h5/make_misfit_statistics.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import tables as pt
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("misfit_h5file" )
+parser.add_argument("out_csv")
+parser.add_argument("--type", default="source")
+parser.add_argument("--stage", default=0, type=int)
+parser.add_argument("--iter", default=0, type=int)
+args = parser.parse_args()
+
+with pt.open_file(args.misfit_h5file, "r") as h5f:
+    if "/window" not in h5f:
+        msg = f"No /window in {args.misfit_h5file}"
+        raise Exception(msg)
+    tbl = h5f.get_node("/window")
+
+    stats = []
+    for win_id in sorted(set(tbl.cols.id)):
+        windows = tbl.read_where(f"id == {win_id}")
+        cc0 = windows['cc0']
+        weights = windows['weight']
+
+        weight_sum = sum(weights)
+        wcc0_sum = sum(weights * cc0)
+        wcc0_avg = wcc0_sum / weight_sum
+
+        data = {}
+        data['type'] = args.type
+        data['stage'] = args.stage
+        data['iter'] = args.iter
+        data['id'] = win_id.decode()
+        data["weight_sum"] = weight_sum
+        data["wcc0_sum"] = wcc0_sum
+        data["wcc0_avg"] = wcc0_avg
+
+        # print(net, sta, win_id, nwin, np.mean(cc0))
+        stats.append(data)
+    
+    df = pd.DataFrame(stats)
+    df.to_csv(args.out_csv, float_format="%.4f", index=False)
---
+++ utils/misfit_dev_h5/run_misfit_statistics.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+work_dir=${1:?[arg] work_dir, e.g. ./}.
+inv_type=${2:?[arg] inv_type, e.g. source}
+stage_num=${3:?[arg] stage_num, e.g. 0}
+max_niter=${4:?[arg] max_niter, e.g. 0}
+event_list=${5:?[arg] events.lst, e.g. events.lst}
+
+num=$(echo $stage_num | awk '{printf "%02d",$1}')
+stage_dir=${work_dir}/stage${num}_${inv_type}
+for iter_num in $(seq 0 $max_niter)
+do
+  num=$(echo $iter_num | awk '{printf "%02d",$1}')
+  iter_dir=$stage_dir/iter${num}
+
+  for event_name in $(awk '$1!~/^#/{print $1}' $event_list)
+  do
+    misfit_dir=$iter_dir/events/$event_name/misfit
+    if [ ! -f $misfit_dir/misfit.h5 ]; then
+      continue
+    fi
+    echo python sem_utils/misfit/misfit_statistics.py \
+      $misfit_dir/misfit.h5 \
+      $misfit_dir/misfit.csv \
+      --stage $stage_num --iter $iter_num --type $inv_type
+  done
+done

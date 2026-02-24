1b5dda4b3a79a041a88edb110e6c7187697abcdb
Kai Tao <taotaokai@gmail.com>
2026-02-24 16:55:03 +0800

    update

--- meshfem3d/sem_get_average_depth_profile.py
+++ meshfem3d/sem_get_average_depth_profile.py
@@ -209,7 +209,7 @@ def sampling_1D_profiles_over_regular_xi_eta_grid(
     data["lon"] = grd3_lon.flatten()
     data["depth_km"] = grd3_dep.flatten() / 1000.0
     for i, tag in enumerate(model_names):
-        data[tag] = interp_model[:, i]
+        data[tag] = interp_model[:, i].flatten()
     data["status"] = final_status.flatten()
     df = pd.DataFrame.from_dict(data)
     df.to_csv(out_csv)

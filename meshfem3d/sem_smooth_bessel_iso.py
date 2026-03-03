eaf5e0fd2c38cfa8cb332736d7e5e621a2bbbcd1
Kai Tao <taotaokai@gmail.com>
2026-03-03 16:37:53 +0800

    update

---
+++ meshfem3d/sem_smooth_bessel_iso.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""smoothing specfem3D GLL model in the lateral and vertical directions using Gaussian window of spatially varying size
+
+smooth the model using Gaussian kernel by solving the heat equation
+
+Gauss(r,sigma) = exp(- 0.5 * r^2 / sigma^2) / (2 * PI * sigma^2)^(d/2)   (d = 3 for 3-D, 2 for 2-D)
+
+Heat equation:
+
+Du/Dt - k * D^2(u)/Dx^2 = 0
+
+I.C. u(t=0) = m (model before smoothing)
+B.C. n .dot. Du/Dx = 0 (adiabatic boundary condition)
+
+Fundamental solution: P(x, t; k) = exp(-r^2/(4kt)) / (4*PI*k*t)^(d/2) for whole space
+
+for k = 0.5 * sigma^2 and t = 1: P(x,1;k) = Gauss(r,sigma)
+
+Since the solution of the intitial value problem is u(t) = P(x, t) * u(x, t=0) (* is spatial convolution)
+
+, it approximates the smoothing by convolution with a Gaussian window except difference near the boundary:
+
+(e.g. initial value is evenly extended from the boundary in 1-D case)
+
+"""
+import sys
+import time
+import argparse
+
+import numpy as np
+
+from scipy.io import FortranFile
+
+from mpi4py import MPI
+
+from meshfem3d_constants import R_EARTH_KM
+
+from meshfem3d_utils import (
+    sem_mesh_read,
+    sem_mesh_get_vol_gll,
+    sem_mesh_mpi_read,
+    gll2glob,
+    laplacian_iso,
+    assemble_MPI_scalar,
+    get_gll_weights,
+    write_gll_file,
+)
+
+
+# ====== parameters
+parser = argparse.ArgumentParser()
+
+parser.add_argument("nproc", type=int)  # number of slices
+parser.add_argument(
+    "mesh_dir", help="e.g. DATABASES_MPI/"
+)  # <mesh_dir>/*_solver_data[_mpi].bin
+parser.add_argument("model_dir", help="e.g. DATABASES_MPI/")  #
+parser.add_argument(
+    "model_name", help="e.g. vsv"
+)  # <model_dir>/proc***_<model_name>.bin
+parser.add_argument(
+    "smooth_length",
+    type=float,
+    help="full width at half maximum (FWHM) of Gaussian kernel in km",
+)
+parser.add_argument("out_dir")  # num of processes
+# control parameters for CG solver
+parser.add_argument(
+    "--max_iter", type=int, default=100, help="maximum number of iteration"
+)
+parser.add_argument(
+    "--max_tolerance",
+    type=float,
+    default=1e-5,
+    help="relative residual to stop iteration",
+)
+parser.add_argument(
+    "--debug",
+    action="store_true",
+    help="output immediate results for debugging",
+)
+
+args = parser.parse_args()
+print(args)
+
+nproc = args.nproc
+mesh_dir = args.mesh_dir  # <mesh_dir>/proc******_solver_data[_mpi].bin
+model_dir = args.model_dir  # <model_dir>/proc******_<model_name>.bin
+model_name = args.model_name  # e.g. vpv,vsv,rho,qmu,qkappa
+smooth_length = args.smooth_length
+out_dir = args.out_dir
+max_iter = args.max_iter
+max_tolerance = args.max_tolerance
+
+smooth_length /= R_EARTH_KM  # non-dimensionalize as in SEM
+kappa = (0.5 * smooth_length)**2
+
+# ====== smooth each target mesh slice
+comm = MPI.COMM_WORLD
+mpi_size = comm.Get_size()
+mpi_rank = comm.Get_rank()
+
+if mpi_size != nproc:
+    raise Exception(f"{mpi_size=} must euqal {nproc=}!")
+
+if mpi_rank == 0:
+    tic = time.time()
+
+# --- read in SEM mesh slice
+mesh_file = "%s/proc%06d_reg1_solver_data.bin" % (mesh_dir, mpi_rank)
+mesh = sem_mesh_read(mesh_file)
+
+mesh_file = "%s/proc%06d_reg1_solver_data_mpi.bin" % (mesh_dir, mpi_rank)
+mesh_mpi = sem_mesh_mpi_read(mesh_file)
+
+nspec = mesh["nspec"]
+nglob = mesh["nglob"]
+gll_dims = mesh["gll_dims"]
+ibool = mesh["ibool"]
+
+model_file = "%s/proc%06d_reg1_%s.bin" % (model_dir, mpi_rank, model_name)
+with FortranFile(model_file, "r") as f:
+    model_gll = np.reshape(f.read_ints(dtype="f4"), gll_dims)
+
+# print(f'{mpi_rank=}, {mesh_mpi["my_neighbors"]=}')
+
+comm.Barrier()
+if mpi_rank == 0:
+    elapsed_time = time.time() - tic
+    print(f"====== finish reading mesh/model, {elapsed_time=}")
+    sys.stdout.flush()
+
+dv_gll = sem_mesh_get_vol_gll(mesh)
+dv_glob = gll2glob(
+    dv_gll,
+    mesh["nglob"],
+    mesh["ibool"],
+)
+assemble_MPI_scalar(
+    dv_glob,
+    mesh_mpi["num_interfaces"],
+    mesh_mpi["max_nibool_interfaces"],
+    mesh_mpi["nibool_interfaces"],
+    mesh_mpi["ibool_interfaces"],
+    mesh_mpi["my_neighbors"],
+)
+
+u_dv_glob = gll2glob(
+    model_gll * dv_gll,
+    nglob,
+    ibool,
+)
+assemble_MPI_scalar(
+    u_dv_glob,
+    mesh_mpi["num_interfaces"],
+    mesh_mpi["max_nibool_interfaces"],
+    mesh_mpi["nibool_interfaces"],
+    mesh_mpi["ibool_interfaces"],
+    mesh_mpi["my_neighbors"],
+)
+
+u_glob = u_dv_glob / dv_glob
+
+if args.debug:
+    out_file = "%s/proc%06d_reg1_%s.bin" % (out_dir, mpi_rank, model_name)
+    write_gll_file(out_dir, "dv", mpi_rank, dv_glob[ibool])
+    write_gll_file(out_dir, f"{model_name}_in", mpi_rank, u_glob[ibool])
+
+
+comm.Barrier()
+
+# GLL points weights
+zgll, wgll, dlag_dzgll = get_gll_weights()
+
+#--- backward Euler
+# M * (u_{n+1} - u_n) = dt * K * u_{n+1}
+# (M - dt * K) * u_{n+1} = M * u_n
+# (1 - dt * M^{-1} * K) * u_{n+1} = u_n
+
+def Kx(x_glob):
+    kx_glob = laplacian_iso(
+        x_glob,
+        kappa,
+        wgll,
+        dlag_dzgll,
+        mesh["ibool"],
+        mesh["dxsi_dx"],
+        mesh["dxsi_dy"],
+        mesh["dxsi_dz"],
+        mesh["deta_dx"],
+        mesh["deta_dy"],
+        mesh["deta_dz"],
+        mesh["dgam_dx"],
+        mesh["dgam_dy"],
+        mesh["dgam_dz"],
+        mesh["jacobian"],
+    )
+
+    assemble_MPI_scalar(
+        kx_glob,
+        mesh_mpi["num_interfaces"],
+        mesh_mpi["max_nibool_interfaces"],
+        mesh_mpi["nibool_interfaces"],
+        mesh_mpi["ibool_interfaces"],
+        mesh_mpi["my_neighbors"],
+    )
+
+    return kx_glob
+
+
+def solve_cg(u):
+    def Ax(x_glob):
+        return x_glob - Kx(x_glob) / dv_glob
+    b = u
+
+    x = u.copy()
+    r = b - Ax(x)
+    p = r.copy()
+    rsold = comm.allreduce(sum(r**2), op=MPI.SUM)
+    # x = np.zeros_like(b)
+    iter = 0
+    threshold = rsold * max_tolerance
+    pAp = 0.0
+    while rsold > threshold:
+        if iter > max_iter:
+            print(
+                f"{mpi_rank=}: stop iteration at {max_iter=}, {rsold=}, {pAp=}. "
+                f"unconvergence might occur, consider increase number of steps! {nt=}"
+            )
+            break
+        # print(f"{mpi_rank=}, {rsold=}")
+        # for i in range(100):
+        Ap = Ax(p)
+        pAp = comm.allreduce(sum(p * Ap), op=MPI.SUM)
+        alpha = rsold / pAp
+        x = x + alpha * p
+        r = r - alpha * Ap
+        rsnew = comm.allreduce(sum(r**2), op=MPI.SUM)
+        p = r + (rsnew / rsold) * p
+        rsold = rsnew
+        iter += 1
+        # if mpi_rank == 0:
+        #     i += 1
+        #     print(f"{i=}, {rsold=}, {pAp=}")
+        #     sys.stdout.flush()
+    return x
+
+
+if mpi_rank == 0:
+    elapsed_time = time.time() - tic
+    print(f"====== start iterative smoothing (diffusion), {elapsed_time=}")
+    sys.stdout.flush()
+
+u = u_glob
+
+max_u = comm.reduce(max(u), op=MPI.MAX, root=0)
+min_u = comm.reduce(min(u), op=MPI.MIN, root=0)
+if mpi_rank == 0:
+    elapsed_time = time.time() - tic
+    # print(f"{it=}, {max(abs(mu))=}, {max(abs(ku))=}, {max(abs(b))=}, {max(abs(u))=}, {elapsed_time=}")
+    print(f"initial: {min_u=}, {max_u=}, {elapsed_time=}")
+    sys.stdout.flush()
+
+# apply Bessel filter twice
+u = solve_cg(u)
+u = solve_cg(u)
+
+max_u = comm.reduce(max(u), op=MPI.MAX, root=0)
+min_u = comm.reduce(min(u), op=MPI.MIN, root=0)
+if mpi_rank == 0:
+    elapsed_time = time.time() - tic
+    # print(f"{it=}, {max(abs(mu))=}, {max(abs(ku))=}, {max(abs(b))=}, {max(abs(u))=}, {elapsed_time=}")
+    print(f"after smoothing: {min_u=}, {max_u=}, {elapsed_time=}")
+    sys.stdout.flush()
+
+comm.Barrier()
+
+# write out smoothed model files
+out_file = "%s/proc%06d_reg1_%s.bin" % (out_dir, mpi_rank, model_name)
+with FortranFile(out_file, "w") as f:
+    # f.write_record(np.array(u_glob[ibool], dtype="f4"))
+    f.write_record(np.array(u[ibool], dtype="f4"))
+
+if mpi_rank == 0:
+    elapsed_time = time.time() - tic
+    print(f"====== finish writing out smoothed model files, {elapsed_time=}")
+    # print(f"{du_glob=}, {du_glob.dtype=}")
+
+comm.Barrier()

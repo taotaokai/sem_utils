27ade04a27544174c3ec233db8a40c9476757259
Kai Tao <taotaokai@gmail.com>
2026-03-07 21:05:43 +0800

    update

--- utils/misfit_dev_h5/run_misfit_statistics.sh
+++ utils/misfit_dev_h5/run_misfit_statistics.sh
@@ -3,12 +3,13 @@
 work_dir=${1:?[arg] work_dir, e.g. ./}
 inv_type=${2:?[arg] inv_type, e.g. source}
 stage_num=${3:?[arg] stage_num, e.g. 0}
-max_niter=${4:?[arg] max_niter, e.g. 0}
-event_list=${5:?[arg] events.lst, e.g. events.lst}
+min_niter=${4:?[arg] min_niter, e.g. 0}
+max_niter=${5:?[arg] max_niter, e.g. 1}
+event_list=${6:?[arg] events.lst, e.g. events.lst}
 
 num=$(echo $stage_num | awk '{printf "%02d",$1}')
 stage_dir=${work_dir}/stage${num}_${inv_type}
-for iter_num in $(seq 0 $max_niter)
+for iter_num in $(seq $min_niter $max_niter)
 do
   num=$(echo $iter_num | awk '{printf "%02d",$1}')
   iter_dir=$stage_dir/iter${num}
